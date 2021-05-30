7164119a377884759964b40356da51fb63b4d116
변웅섭 <woungsub1234@gmail.com>
2021-05-31 02:04:02 +0900

    feat: Add User model class

---
+++ app/model/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, Text, DateTime
+from sqlalchemy.sql import func
+
+
+from .base import Base
+
+DEFAULT_PROFILE_IMAGE = "https://image.shutterstock.com/image-vector/user-icon-trendy-flat-style-260nw-418179865.jpg"  # 추후 AWS S3 url로 수정, schema 파일로 이동
+
+
+class User(Base):
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    email = Column(String(100), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    nickname = Column(String(100), nullable=False, unique=False)
+    profile_image = Column(Text, default=DEFAULT_PROFILE_IMAGE)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
