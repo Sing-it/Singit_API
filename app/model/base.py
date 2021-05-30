39e8c2149e8127a13d11b21ebef20d13e551bb4d
변웅섭 <woungsub1234@gmail.com>
2021-05-31 02:42:28 +0900

    chore: Add index pk on base model & Change tablename

--- app/model/base.py
+++ app/model/base.py
@@ -1,13 +1,13 @@
 from typing import Any
-
+from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
 class Base:
-    id: Any
     __name__: str
+    id = Column(Integer, autoincrement=True, primary_key=True)
 
     @declared_attr
     def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+        return cls.__name__
