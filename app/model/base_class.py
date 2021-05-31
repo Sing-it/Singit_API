6a4354b8c022746e6a9df575e3e3b0b0510866eb
변웅섭 <woungsub1234@gmail.com>
2021-05-31 22:02:19 +0900

    chore: Move base model class to base_class.py

---
+++ app/model/base_class.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+
+
+@as_declarative()
+class Base:
+    __name__: str
+    id = Column(Integer, autoincrement=True, primary_key=True)
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__
