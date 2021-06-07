44648de24fe3eb50122272c197f5c096dfa97fbb
변웅섭 <woungsub1234@gmail.com>
2021-06-07 23:19:37 +0900

    feat: Add is_active column on User model

--- app/model/user.py
+++ app/model/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Text, DateTime, Integer
+from sqlalchemy import Column, String, DateTime, Integer, Boolean
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
@@ -15,17 +15,18 @@ class User(Base):
     profile_image = Column(String(100), default=DEFAULT_PROFILE_IMAGE)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
+    is_active = Column(Boolean, default=False)
     user_artist = relationship("UserArtist", uselist=False, back_populates="User")
 
     def __repr__(self) -> str:
-        return "<{}(id='{}', email='{}', password='{}', nickname='{}', profile_image='{}', created_at='{}', updated_at='{}')>".format(
+        return "<{}(id='{}', email='{}', password='{}', nickname='{}', profile_image='{}', is_active='{}'created_at='{}', updated_at='{}')>".format(
             self.__name__,
             self.id,
             self.email,
             self.password,
             self.nickname,
             self.profile_image,
+            self.is_active,
             self.created_at,
             self.updated_at,
         )
