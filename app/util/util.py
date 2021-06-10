43924afbdd435ae08c7569c7369ef8a5e85b18d2
변웅섭 <woungsub1234@gmail.com>
2021-06-10 20:35:21 +0900

    feat: Add path param in s3 module

--- app/util/util.py
+++ app/util/util.py
@@ -4,12 +4,26 @@ from app.core.config import settings
 
 
 class Util:
-    def s3upload(self, file, filename):
+    def s3upload(self, file, path: str, filename: str):
         """
         :param file: 업로드할 파일
+        :param path: bucket 내부 path 지정
         :param filename: 업로드할 파일의 이름
-        :return: 업로드한 파일의 s3 경로
+        :return: 업로드한 파일의 url
         """
+        paths = [
+            "image/artist/",
+            "image/user/",
+            "image/song/",
+            "sound/material/song/",
+            "sound/material/accompaniment/",
+            "sound/user/sound/",
+            "sound/artist/song/",
+            "sound/artist/sound/",
+        ]
+        if path not in paths:
+            return {"msg": "file kind isn't right"}
+        path += filename
         try:
             s3 = boto3.client(
                 "s3",
@@ -19,13 +33,9 @@ class Util:
             region = settings.AWS_CONFIG["REGION"]
 
             bucket_name = settings.AWS_CONFIG["BUCKET_NAME"]
-            s3.upload_fileobj(
-                file, bucket_name, filename, ExtraArgs={"ACL": "public-read"}
-            )
+            s3.upload_fileobj(file, bucket_name, path, ExtraArgs={"ACL": "public-read"})
 
-            url = "https://s3-{}.amazonaws.com/{}/{}".format(
-                region, bucket_name, filename
-            )
+            url = "https://s3-{}.amazonaws.com/{}/{}".format(region, bucket_name, path)
 
         except Exception as e:
             return e
