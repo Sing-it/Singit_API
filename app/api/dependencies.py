9f049af0f2763bbdb798751685b66781eda04211
변웅섭 <woungsub1234@gmail.com>
2021-06-13 01:01:26 +0900

    feat: Add get_current_user by token

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -1,5 +1,14 @@
 from typing import Generator
 from app.db.sesson import LocalSession
+from sqlalchemy.orm import Session
+from fastapi import Depends, Header, HTTPException, status
+
+import jwt
+from pydantic import ValidationError
+
+
+from app import model, crud, schemas
+from app.core.config import settings
 
 
 def get_db() -> Generator:
@@ -8,3 +17,24 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
+
+
+def get_current_user(
+    db: Session = Depends(get_db()), authorization: str = Header(None)
+) -> model.User:
+    try:
+        if authorization.split()[0] not in ("jwt", "JWT"):
+            raise HTTPException(400, "credentials required")
+        token = authorization.split()[1]
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        token_data = schemas.AccessTokenPayload(**payload)
+
+    except (jwt.JWTError, ValidationError):
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Could not validate credentials",
+        )
+    user = crud.user.get(db, id=token_data.sub)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
