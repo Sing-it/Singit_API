d42882615a99c71c7f85a8607cc217944c29a062
변웅섭 <woungsub1234@gmail.com>
2021-06-13 01:01:54 +0900

    feat: Add verify_password

--- app/util/hash.py
+++ app/util/hash.py
@@ -5,3 +5,7 @@ context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def get_hashed_password(plain_password: str) -> str:
     return context.hash(plain_password)
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return context.verify(plain_password, hashed_password)
