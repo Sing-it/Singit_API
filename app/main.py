59c33f8dc04d061f6a8ae76850d7ed61b8fe8c1c
변웅섭 <woungsub1234@gmail.com>
2021-06-16 23:55:00 +0900

    chore: Add api router on main file

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.api.router import api_router
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 async def test():
     return {"message": "Server Listening"}
+
+
+app.include_router(api_router)
